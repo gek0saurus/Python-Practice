105f6538aeaf2c7ac7d06f61c0b735ec662f7543
gek0saurus <37968388+gek0saurus@users.noreply.github.com>
2019-06-10 19:31:35 -0400

    Add files via upload

---
+++ BitMapWriter.py
@@ -0,0 +1,96 @@
+
+
+
+def LittleEndianToInt(Hex):
+    byte = ""
+    text = ""
+    length = int(len(Hex))
+    for i in range(int(length/2)):
+        byte =  Hex[length-(i+1)*2:length-(i+1)*2+2]
+        text = text + byte
+    return(int(text,16))
+
+def IntToLittleEndian(Integer):
+    Hex = hex(Integer)
+    Hex = Hex[2:] #trims off the "0x"
+    length = len(Hex)
+    string = ""
+    if length % 2 == 1:
+        Hex = "0" + Hex
+        length = len(Hex) #adds a 0 to the last byte if length is odd number
+    for i in range(int(length/2+1.5)):
+        string = string + Hex[length - i*2:length - (i-1) * 2]
+    return string
+        
+
+
+def CreateNewTwentyFourBit(grid):
+    NewBitmap = open("NewBitmap.bmp","wb")
+    #Header
+    cellwidth = 16
+    fullhexstring = ""
+    header = ""
+    body = ""
+    hexlist = []
+    signature = "424d" #BM
+    filesize =""
+    reserved = "00000000"
+    offbits = "32000000"
+    headersize = "28000000"
+    width = "" 
+    height = "" 
+    planes = "0100"
+    bitcount = "1800"#24bit
+    compression = "00000000"
+    imagesize = ""
+    Xpixelspermeter = "00000000"
+    Ypixelspermeter = "00000000"
+    clrused = "00000000"
+    clrimportant = "00000000"
+
+    width = IntToLittleEndian(grid.width*cellwidth)
+    #could create a function for these
+    for i in range(8- len(width)):
+        width = width + "0" # fill in 0s
+    height = IntToLittleEndian(grid.height*cellwidth)
+    for i in range(8- len(height)):
+        height = height + "0" # fill in 0s
+    filesize = IntToLittleEndian((grid.width * grid.height)*cellwidth**2*3+54)
+    for i in range(8- len(filesize)):
+        filesize = filesize + "0" # fill in 0s
+    imagesize = IntToLittleEndian((grid.width * grid.height)*cellwidth**2*3)
+    for i in range(8- len(imagesize)):
+        imagesize = imagesize + "0"
+    
+    header = signature + filesize + reserved + offbits + headersize + width + height +  planes\
+            + bitcount + compression + imagesize + Xpixelspermeter + Ypixelspermeter\
+            + clrused + clrimportant
+
+    #Body
+    for row in grid[::-1]:
+        for cell in row:
+            if cell.ID == 1:
+                colorhex = "000000"
+            else:
+                colorhex = "ffffff"
+            body = body + colorhex
+##        body = body + "0000" #padding
+
+    #Expand Body
+    #Turns each cell into a cellwidth*cellwidth square
+    expandedbody = ""
+    for h in range(grid.height*cellwidth):
+        for w in range(grid.width):
+            expandedbody = expandedbody + body[(int(h/cellwidth)*grid.width)*6+w*6:\
+                                        (int(h/cellwidth)*grid.width)*6+w*6+6]*cellwidth
+
+    #Compile
+    fullhexstring = header + expandedbody
+    hexlist = [int(fullhexstring[i:i+2],16) for i in range(0, len(fullhexstring), 2)]
+    array = bytearray(hexlist)
+    NewBitmap.write(array)
+    NewBitmap.close
+    print("File saved")
+
+
+#LittleEndianToInt(str(32000000))
--- GridMove.py
+++ GridMove.py
@@ -1,106 +1,193 @@
 import random
+import PathfinderCharacterMaker
+import BitMapWriter
+
+
 #----------------------------------------------------------
+
+
+class grid (list):
+    def __init__(self, width, height):
+        self.width = width
+        self.height = height
+
+    def return_list(self):
+        gridlist = []
+        for row in self:
+            gridlist.append(row)
+        print(gridlist)
+        print("hi")
+        return gridlist
+
+    def create_blank(self):
+        print("Map created")
+        for i in range (0,self.height):
+            row = []
+            for j in range(0,self.width):
+                row.append(cell(0))#Blank cell
+            self.append(row)
+
+    def create_random(self, num_walls):
+        self.create_blank()
+        print("Map randomized")
+        for i in range(0, num_walls):
+            self.editcell(random.randint(0, self.height-1),
+            random.randint(0,self.width-1), cell(1))#wall cell
+
+    def create_custom(self, mapstring):
+        pass
+            
+    def print(self, playerlist):
+        print("Printing Map")
+        ##Place players
+        for i in range(0,len(playerlist)):
+            try:
+                self.editcell(playerlist[i].position[0],
+                              playerlist[i].position[1],
+                              str(" "+str(playerlist[i].name[:1])+"  "))
+
+            except:
+                print("cannot place player " + playerlist[i].name)
+        #Print map
+        text = "" 
+        for row in self:
+            text = ":"
+            for cell in row:
+                text = text + str(cell)
+            print(text)
+            text = ""
+
+        #Create Bitmap
+        BitMapWriter.CreateNewTwentyFourBit(self)
+        
+        #Clear trails
+        for i in self:
+            for j in i:
+                try:
+                    if j.ID == 3:#triailcell
+                        j.ID = 0#blank cell
+                except:
+                    pass
+
+    def editcell(self, x, y, newvalue):
+        try:
+            self[x][y] = newvalue
+        except:
+            print("Out of range")
+#------------------------------------------------------------
+
+
 class cell (object):
     def __init__(self,ID):
         assert type(ID) == int
         self.ID = ID
+
+    def __repr__(self):
+        returntext = self.celloptions(self.ID)
+        return returntext
+
+    def __str__(self):
+        returntext = self.celloptions(self.ID)
+        return returntext
+
     def celloptions(self,option):
         options = {
-            1: '#', #wall cell
+            0: '██',
+            1: '▒▒', #wall cell
             2: 'O', #player cell
-            3: '.', #trail cell
+            3: '▓▓', #trail cell
             4: 'X', #goal cell
         }
         return options.get(option, ' ')
-    def __str__(self):
-        return self.celloptions(self.ID)
+    
     def destroy(self):
         self.ID = 0
-#----------------------------------------------------------    
-def PrintMap(position,X):
-    X[position[0]][position[1]] = cell(2)#playercell
-    text = ""
-    for list in X:
-        for string in list:
-           text = text + str(string)
-        print(text)
-        text = ""
 #----------------------------------------------------------
-def CreateMap(x,y,grid,numwalls):
-    Y = []
-    Goal = []
-    grid.clear()
-    for i in range(0,y):
-        Y.append(cell(0))#blankcell
-    for i in range(0,x):
-        grid.append(Y[:])
-    for i in range(0,numwalls):
-        grid[random.randint(0,x-1)][random.randint(0,y-1)] = cell(1)#wallcell
-    Goal = [random.randint(0,x-1),random.randint(0,y-1)]
-    grid[Goal[0]][Goal[1]] = cell(4)#goalcell
-    return Goal
+
+
+class Unit (object):
+    def __init__(self, name, position, movement):
+        self.name = name
+        self.position = position
+        self.movement = movement
+
+    def __repr__(self):
+        return self.name
+
+    def __str__(self):
+        return self.name
+
+    def Move(self, grid, directionstring, x, y):
+        for j in directionstring:
+            grid[int(self.position[0])][int(self.position[1])] = cell(3)#trailcell
+            self.position = ObstacleCheck(grid,self.position,j,x,y)
+        return grid
+                    
+
+#-----------------------------------------------------------
+class Player(Unit):
+    pass
+
+
 #----------------------------------------------------------
-def move(grid,position,x,y):
+class NPC(Unit):
+    pass
+
+
+#----------------------------------------------------------
+class Monster(Unit):
+    pass
+
+
+#-------------------------------------------------------------
+def CreatePlayer(name, position, movement, playerlist):
+    print("Created player " + str(name) + " at coordinates " + str(position))
+    player = Player(name,position,movement)
+    playerlist.append(player)
+    return playerlist
+
+
+#----------------------------------------------------------
+def ObstacleCheck(grid, position, direction, x, y):
     def moveoptions(option):
         options = {
             #[(0 = verticle 1 = horizontal), increment]
-            "w": [0,-1], #Up
-            "a": [1,-1], #Left
-            "s": [0,1], #Down
-            "d": [1,1], #Right
+            "w": [-1, 0], #Up
+            "a": [0, -1], #Left
+            "s": [1, 0], #Down
+            "d": [0, 1], #Right
         }
         return options.get(option, " ")
-    def detectcollision(position,destination):
+
+    def detectcollision(position, destination):
         if destination[0] < 0\
         or destination[1] < 0\
         or destination[0] > x-1\
-        or destination[1] > y-1:
-            print("You cannot go that way")
-        if grid[destination[0]][destination[1]].ID == 1:#wallcell
-            print("You cannot go that way")
+        or destination[1] > y-1\
+        or grid[destination[0]][destination[1]].ID == 1:#wallcell
+            print("Bump")
         else:
             position = destination[:]
         return position
-    grid[position[0]][position[1]] = cell(3)#trailcell
-    move = moveoptions(input("Up(w),Left(a), Down(s), or Right(d)?"))
+    move = moveoptions(direction)
     if move != " ":
         destination = position[:]
-        destination[move[0]] += move[1]
+        destination[0] += move[0]
+        destination[1] += move[1]
         position = detectcollision(position,destination)
+        
     else:
         print("invalid input")
     return position
 #----------------------------------------------------------
-def main():
-    y = 10
-    x = 10
-    walls = 20
-    grid = []
-    i = 0
-    position = [0,0]
-    userinput = ""
-    playagain = True
 
-    while playagain:
-        Goal = CreateMap(x,y,grid,walls)
-        while position != Goal:
-            PrintMap(position,grid)
-        ## move input
-            position = move(grid,position,x,y)
-        ## win
-        print("Success!")
-        PrintMap(position,grid)
-        while userinput != "Y" and userinput != "N":
-            userinput = input("Play again? Y/N")
-            if userinput == "Y":
-                playagain = True
-            elif userinput == "N":
-                playagain = False
-            else:
-                print("Invalid Input.")
-        userinput = ""   
-#----------------------------------------------------------
-main()
+##----------------------------------------------------------
 
-## end
-print("Good bye <3")
+
+NewMap = grid(20, 20)
+NewMap.create_random(80)
+NewMap.print([])
+
+
+
+
